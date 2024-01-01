a39c48f11e59e0ef4452ab27c4ae54e62cefd117
Ivan Mihaylov <ivan4o03.it@gmail.com>
2024-01-01 23:29:03 +0200

    Add text manipulation commands

--- src/Swiss-Cli/Commands/TextCommands.cs
+++ src/Swiss-Cli/Commands/TextCommands.cs
@@ -1,4 +1,5 @@
-﻿using Swiss_Cli.Extensions;
+﻿using Cocona;
+using Swiss_Cli.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,52 @@ namespace Swiss_Cli.Commands
 {
     public class TextCommands : ICommand
     {
-        public void Uppercase()
+        [Command("upper")]
+        public void Uppercase([Argument] string text)
         {
-
+            Console.WriteLine(text.ToUpper());
         }
 
-        public void LowerCase()
+        [Command("lower")]
+        public void LowerCase([Argument] string text)
         {
+            Console.WriteLine(text.ToLower());
+        }
 
+        [Command("title")]
+        public void TitleCase([Argument] string text)
+        {
+            Console.WriteLine(string.Join(' ',text.Split().Select(x =>
+            {
+                var a = x.ToCharArray();
+                if (a.Length == 0)
+                {
+                    return string.Empty;
+                }
+                a[0] = char.Parse(a[0].ToString().ToUpper());
+                return string.Join(string.Empty, a);
+            })));
+        }
+
+        [Command("reverse")]
+        public void Reverse([Argument] string text)
+        {
+            Console.WriteLine(string.Join(string.Empty, text.Reverse()));
+        }
+
+        [Command("is-palindrome")]
+        public void IsPalindrome([Argument] string text)
+        {
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                if (text[i] != text[text.Length - i - 1])
+                {
+                    Console.WriteLine(false);
+                    return;
+                }
+            }
+
+            Console.WriteLine(true);
         }
     }
 }
